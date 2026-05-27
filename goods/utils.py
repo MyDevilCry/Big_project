dc6b4efed17de66674c2952c71fbd3e73a006f14
1PROJ <jane2cassidy@gmail.com>
2026-05-27 13:59:15 +0300

    Зробив пошук + виправив міграції з JSON

--- goods/utils.py
+++ goods/utils.py
@@ -1,13 +1,14 @@
 from goods.models import Products
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery,SearchRank
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    keywords = [word for word in query.split() if len(word) > 2] #Це ключи для слів по яким буде пошук.
-    q_objects = Q()
+    vector = SearchVector('name','description')
+    query = SearchQuery(query)
 
-    for token in keywords: #Робимо умову для слова в цьому ключі де містить пошук
-        q_objects |= Q(description__icontains=token) |Q (name__icontains=token) #Обов'язково ДОДАЄМО | для того щоб
-        #джанго розумів що пошук має бути і за описом і за ім'ям
-    return Products.objects.filter(q_objects)
+
+    return Products.objects.annotate(rank=SearchRank(vector,query)).order_by('-rank')#Таким чином ми додаємо розумний пошук
+
+
+
