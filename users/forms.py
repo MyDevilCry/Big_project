7b914881d2daf2ec2456d6c11a04326d8dda8943
1PROJ <jane2cassidy@gmail.com>
2026-06-03 20:08:06 +0300

    Поправив шаблон

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,User
 #forms.model.Form дозволяє РЕДАГУВАТИ вже готові дані
 #attrs потрібен для того, щоб передати звичайні HTML-атрибути (такі як class, placeholder, id, тощо) з твого Python-коду прямо всередину HTML-тегу, який згенерує Django.
-class UserLoginForm(AuthenticationForm): #Вона бере те, що ввів користувач, сама звіряє це з базою даних і каже, правильні дані чи ні.
+class UserLoginForm(AuthenticationForm): #Наслідуємо спеціальну форму аутентифікації
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваше имя','id': 'username'})
+        label = 'Имя',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваше имя пользователя','id': 'username'}),
     )
-    password = (forms.CharField
-        (widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваш пароль','id': 'password'})
-    ))
+    password = forms.CharField(
+        label = 'Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваш пароль','id': 'password'})
+    )
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username',
+                  'email',
+                  'first_name',
+                  'last_name',
+        )
+
+
