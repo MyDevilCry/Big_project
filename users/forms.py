4096ebdb9261d7459fd5bfdc1b713af0ae3fb9ba
1PROJ <jane2cassidy@gmail.com>
2026-06-07 15:42:16 +0300

    Виправив HTML + зробив правки у контролері

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,get_user_model
+
+User = get_user_model()
 #forms.model.Form дозволяє РЕДАГУВАТИ вже готові дані
 #attrs потрібен для того, щоб передати звичайні HTML-атрибути (такі як class, placeholder, id, тощо) з твого Python-коду прямо всередину HTML-тегу, який згенерує Django.
 class UserLoginForm(AuthenticationForm): #Наслідуємо спеціальну форму аутентифікації
@@ -20,5 +22,19 @@ class UserRegisterForm(UserCreationForm):
                   'first_name',
                   'last_name',
         )
+
+
+    def save(self, commit=True): #commit=True — це наказ. Він означає: "За замовчуванням, як тільки дані оброблені, відразу запиши їх у базу даних".
+        user = super().save(commit=False) #Цим ми кажемо Django:
+# "Візьми дані з форми (username, email, first_name, last_name), створи з них тимчасовий об'єкт користувача в пам'яті комп'ютера, але поки що не записуй його в базу даних".
+        user.set_password(self.cleaned_data['password1']) #Тут ми робимо так щоб коли користувач ввів пароль, ця строка його зашифрувала
+        if commit: #якщо я викликаю метод як form.save(), то commit автоматично дорівнює True
+            user.save()
+
+
+
+
+
+
 
 
